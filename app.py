d68f11c305bed72f0a97042f69ed433c80fd4740
jonathanyongamg <41997952+jonathanyongamg@users.noreply.github.com>
2018-12-10 01:14:55 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+
+print("Risk Analysis Profile")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
+
+num1 = input("Enter your age: ")
+num2 = input("Enter your father's age: ")
+num3 = input("Enter your mother's age: ")
+result = num1 + num2
+
+print(result)
